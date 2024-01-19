869bb23ba09b4a657187b6feea86f80a159ce3ff
Gabriel <gabriel@mazure.fr>
2024-01-19 19:08:26 +0100

    Optimised calculation by removing useless space

--- main.js
+++ main.js
@@ -29,8 +29,8 @@ class complex {
 window.onload = function() {
     canvas = document.getElementById("viewport");
     const ctx = canvas.getContext("2d");
-    const size = Number(document.getElementById("viewport").getAttribute("width"));
-    let imageData = ctx.getImageData(0,0,size,size);  
+    const size = [Number(document.getElementById("viewport").getAttribute("width")), Number(document.getElementById("viewport").getAttribute("height"))];
+    let imageData = ctx.getImageData(0,0,size[0],size[1]);  
     let precision = 200;
     generate_initial(size,imageData, ctx, precision);
 
@@ -43,7 +43,7 @@ function paint_canvas(data,context){
 }
 
 function set_pixel(imgData,x,y,r,g,b,size){
-    const offset = (y * size + x) * 4;
+    const offset = (y * size[0] + x) * 4;
     imgData.data [offset] = r  ;
     imgData.data [offset + 1] = g;
     imgData.data [offset + 2] = b;
@@ -67,9 +67,9 @@ function check_mandelbrot(c, precision){
 }
 
 function generate_initial(size,imageData,ctx,precision){
-    for (let y = 0; y <= size; y++){
-        for (let x = 0; x <= size; x++){
-            let c = new complex(-2+4*(x/size),-2+4*(y/size));
+    for (let y = 0; y <= size[1]; y++){
+        for (let x = 0; x <= size[0]; x++){
+            let c = new complex(-2+2.7*(x/size[0]),-(7/6)+(14/6)*(y/size[1]));
             let r, g, b;
             let result = check_mandelbrot(c, precision);
             if (result == -1){
