da36f19c5de70601cd36f7b6968351f8377b94e9
Gabriel <gabriel@mazure.fr>
2024-01-24 17:18:45 +0100

    now generates julia when clixing the canvas

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 "use strict";
-let canvas;
+let mandelbrotCanvas;
 
 class complex {
     constructor(a,b){
@@ -27,16 +27,28 @@ class complex {
 }
 
 window.onload = function() {
-    canvas = document.getElementById("viewport");
-    const ctx = canvas.getContext("2d");
-    const size = [Number(document.getElementById("viewport").getAttribute("width")), Number(document.getElementById("viewport").getAttribute("height"))];
-    let imageData = ctx.getImageData(0,0,size[0],size[1]);  
-    let precision = 200;
-    generate_initial(size,imageData, ctx, precision);
+    mandelbrotCanvas = document.getElementById("viewport");
+    const mandelbrotCTX = mandelbrotCanvas.getContext("2d");
+    
+    const size = get_size()
+    adjust_height(size)
 
+    let mandelbrotImageData = mandelbrotCTX.getImageData(0,0,size[0],size[1]);  
+    let precision = 200;
+
+    generate_initial(size,mandelbrotImageData, mandelbrotCTX, precision);
+    mandelbrotCanvas.addEventListener("click", (event) => select_pixel(event,size,mandelbrotImageData,mandelbrotCTX))
 }
 
+function get_size(){
+    const width = Number(document.getElementById("viewport").getAttribute("width"));
+    const height = Math.floor(width / (81/70))
+    return [width, height]
+}
 
+function adjust_height(size){
+    mandelbrotCanvas.setAttribute("height", size[1])
+}
 
 function paint_canvas(data,context){
     context.putImageData(data,0,0);
@@ -51,8 +63,8 @@ function set_pixel(imgData,x,y,r,g,b,size){
 }
 
 
-function check_mandelbrot(c, precision){
-    let z = new complex(0,0);
+function check_pixel(a, c, precision){
+    let z = new complex(a.a, a.b);
     let k = 0;
     while( k <= precision && z.module()<=2){
         z= z.square().addition(c);
@@ -66,12 +78,67 @@ function check_mandelbrot(c, precision){
     }
 }
 
-function generate_initial(size,imageData,ctx,precision){
+function check_mandelbrot(c, precision){
+    let a = new complex(0,0);
+    return check_pixel(a, c, precision);
+}
+
+function check_julia(a, x, y, size, precision){
+    let c = new complex (-2+4*(x/size[0]),-2+4*(y/size[1]))
+    return check_pixel(c, a, precision)
+}
+
+function get_positional_mandelbrot_complex(x,y,size){
+    return new complex(-2+2.7*(x/size[0]),-(7/6)+(14/6)*(y/size[1]));
+}
+
+function generate_initial(size,mandelbrotImageData,mandelbrotCTX,precision){
     for (let y = 0; y <= size[1]; y++){
         for (let x = 0; x <= size[0]; x++){
-            let c = new complex(-2+2.7*(x/size[0]),-(7/6)+(14/6)*(y/size[1]));
+            let c = get_positional_mandelbrot_complex(x,y,size);
             let r, g, b;
             let result = check_mandelbrot(c, precision);
+            if (result == -1){
+                r = 0;
+                g = 0;
+                b = 0;
+            } else {
+                r = 0;
+                g = 255*(result/precision)
+                b = 0;
+            }
+            set_pixel(mandelbrotImageData,x,y,r,g,b,size);
+            }  
+        }   
+    
+    paint_canvas(mandelbrotImageData, mandelbrotCTX);
+}
+
+function select_pixel(event,size,mandelbrotImageData,mandelbrotCTX){
+    const bounding = mandelbrotCanvas.getBoundingClientRect();
+    const x = Math.floor(event.clientX - bounding.left);
+    const y = Math.floor(event.clientY - bounding.top);
+    const a = get_positional_mandelbrot_complex(x,y,size)
+    console.log("clicked pixel x:", x, " y:",y);
+    set_pixel(mandelbrotImageData,x,y,255,255,255,size);
+    paint_canvas(mandelbrotImageData, mandelbrotCTX)
+    create_julia(a)
+}
+
+function create_julia(a){
+    const juliaCanvas = document.getElementById("julia")
+    const juliaCTX = juliaCanvas.getContext("2d")
+    const size = [juliaCanvas.getAttribute("width"), juliaCanvas.getAttribute("height")]
+    let juliaImageData = juliaCTX.getImageData(0, 0, size[0], size[1])
+    let precision = 200
+    generate_julia(a, size, juliaImageData, juliaCTX, precision)
+}
+
+function generate_julia(a,size,imageData,ctx,precision){
+    for (let y = 0; y <= size[1]; y++){
+        for (let x = 0; x <= size[0]; x++){
+            let r, g, b;
+            let result = check_julia(a,x,y,size,precision);
             if (result == -1){
                 r = 0;
                 g = 0;
@@ -86,5 +153,5 @@ function generate_initial(size,imageData,ctx,precision){
         }   
     
     paint_canvas(imageData, ctx);
+    console.log("zbim")
 }
-
